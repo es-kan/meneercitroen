d6ba1434a9c5202e39976153a57cf353ec43fa1f
Erik <erik@theconditionalpixel.nl>
2017-07-24 13:11:17 +0200

    removed person name validator

--- lunchpot/models.py
+++ lunchpot/models.py
@@ -2,7 +2,6 @@
 import datetime
 
 from django.contrib.auth.models import User
-from django.core.validators import MinLengthValidator
 from django.db import models
 
 from functions import format_currency
