b00f3b2470fa95c2b3904ecc9a74676faff9ebfa
Erik <erik@theconditionalpixel.nl>
2017-07-25 14:03:33 +0200

    Sort context data and display negatives in red

--- lunchpot/views.py
+++ lunchpot/views.py
@@ -17,11 +17,11 @@ class DashboardView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(DashboardView, self).get_context_data(**kwargs)
         if self.request.user.is_superuser:
-            context['lunches'] = LunchEvent.objects.all()
+            context['lunches'] = LunchEvent.objects.all().order_by('-date_modified')
             context['people'] = sorted(Person.objects.all(), key=methodcaller('get_balance'), reverse=True)
             context['grocery_data'] = GroceryEvent.objects.get_data()
             context['total_balance'] = Person.objects.get_total_balance()
         else:
-            context['lunches'] = LunchEvent.objects.with_person(self.request.user.person)
-        context['groceries'] = GroceryEvent.objects.all()
+            context['lunches'] = LunchEvent.objects.with_person(self.request.user.person).order_by('-date_modified')
+        context['groceries'] = GroceryEvent.objects.all().order_by('-date_modified')
         return context
